a048e7fa510e66d066c6e3143dad3962a94ec2ad
gabimarques0 <gabrielamarquezzzz6@gmail.com>
2023-09-13 19:55:38 -0400

    find user

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,16 +17,16 @@ const createUser = async (req, res) => {
     })
 }
 
-/*
+
 const findUsers = async (req, res) => {
-    const     = await User.findAll();
+    const users = await User.findAll();
     try {
-        res.json(    );
+        res.json(users);
     } catch (error) {
         res.status(404).json("Ocorreu um erro na busca!");
     };
 }
-
+/*
 const deleteUser = async (req, res) => {
     const id = parseInt(req.params.id);
     try {
